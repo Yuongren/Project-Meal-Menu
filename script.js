35240b641cb938a31b0585c812fa43c6177d5071
gitYoungren <youngrengitonga@gmail.com>
2022-12-17 20:01:52 +0300

    new elements added in css

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 // Write your code here
-// document.addEventListener('DOMContentLoaded', (evetn) => {
-//     getfoodsdrinksandsnacks()
-// })
 
-// function getFoodsdrinksandsnacks(){
-//     fetch("http://localhost:3000/foodsdrinksandsnacks").then(res => res.json()).then(data => {
-//         console.log(data);
-//         data.forEach(foodsdrinksandsnacks => {
-//             displayFoodsdrinksandsnacks(foodsdrinksandsnacks)
-//         })
-//     })
-// }
 
-function myFunction() {
-    var x = document.createElement("BUTTON");
-    var t = document.createTextNode("Click me");
-    x.appendChild(t);
-    document.body.appendChild(x);
-    alert("Please wait for 20 minutes")
-  }
+// function myFunction() {
+//     var x = document.createElement("BUTTON");
+//     var t = document.createTextNode("Click me");
+//     x.appendChild(t);
+//     document.body.appendChild(x);
+//     alert("Please wait for 20 minutes")
+//   }
 
 const star = document.querySelector(".stars a")
 const stars = document.querySelectorAll(".stars a")
