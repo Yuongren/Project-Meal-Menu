5e7b7c4d1e1f01624615fd15d01a18ebac707918
gitYoungren <youngrengitonga@gmail.com>
2022-12-17 18:37:17 +0300

    new elements added in css

--- script.js
+++ script.js
@@ -12,6 +12,13 @@
 //     })
 // }
 
+function myFunction() {
+    var x = document.createElement("BUTTON");
+    var t = document.createTextNode("Click me");
+    x.appendChild(t);
+    document.body.appendChild(x);
+    alert("Please wait for 20 minutes")
+  }
 
 const star = document.querySelector(".stars a")
 const stars = document.querySelectorAll(".stars a")
